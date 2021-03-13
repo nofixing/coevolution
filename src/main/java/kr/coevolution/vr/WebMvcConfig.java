d313dd1eee7f4d24c9502b7e70b7c99d472672af
nofixing <nofixing@gmail.com>
2021-03-13 15:21:42 +0900

    12345

--- src/main/java/kr/coevolution/vr/WebMvcConfig.java
+++ src/main/java/kr/coevolution/vr/WebMvcConfig.java
@@ -1,11 +1,24 @@
 package kr.coevolution.vr;
 
+import kr.coevolution.vr.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/kr/coevolution/vr/config/auth/SecurityConfig.java
+++ src/main/java/kr/coevolution/vr/config/auth/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2/**", "/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/image/**", "/js/**", "/h2/**", "/h2-console/**", "/assets/**").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
 
